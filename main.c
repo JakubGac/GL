65d74c1ad4cf869e9946079721ba4615f1773139
JakubGac <gacus65@gmail.com>
2014-04-06 00:36:03 +0200

    main

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "read.h"
 #include "automat_komorkowy.h"
 #include "wypisywanie.h"
+#include "przechowywanie.h"
+#include "obrazek.h"
 
 /* The Game of Life */
 
@@ -12,18 +15,18 @@ int main ( int argc, char **argv){
 		wypisz_info();
 
 	} else {
-
+	
 		lista l = NULL;
 
-		readFromFile(argv[1]);
+		l = readFromFile(argv[1],l);
 
 		int r = ReadRows(argv[1]);
 
 		int c = ReadColumns(argv[1]);
 	
-		int ilosc_generacji=20;
-	
-		automat_komorkowy(matrix,r,c,ilosc_generacji,l);
+		int ilosc_generacji=300;
+
+		automat_komorkowy(l->tablica_element,r,c,ilosc_generacji,l);
 
 		zapisz_generacje_do_pliku(l,r,c,argv[2]);
 
