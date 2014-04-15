45da9482ea4f9b3c48c3345c2626b54fd23fa760
JakubGac <gacus65@gmail.com>
2014-04-15 23:13:35 +0200

    dodalem zawijanie

--- libraries/zasada_martwa.c
+++ libraries/zasada_martwa.c
@@ -6,36 +6,60 @@ int zasada_martwa(int **tablica, int r, int c,int *zmienna, int rows, int column
 	int ile_zywych=0;
 
 	if((r-1) >= 0){
-		for(i=c-1 ; i < c+2 ; i++){ /* przeszukanie wiersza nad komórką z pominięciem wiersza powyżej*/
+		for(i=c-1 ; i < c+2 ; i++){ /* przeszukanie wiersza nad komórką z zawinieciem przy wierszy pierwszym */
 			if(i >= 0 && i < columns){
 				if(tablica[r-1][i] == 1){
 					ile_zywych++;
 				}
 			}
 		}
+	} else {
+		for(i=c-1 ; i < c+2 ; i++){ 
+                        if(i >= 0 && i < columns){
+                                if(tablica[rows-1][i] == 1){
+                                        ile_zywych++;
+                                }
+                        }
+                }
 	}
 
 	if((r+1) < rows){
-		for(i=c-1 ; i < c+2 ; i++) { /* przeszukanie wiersza pod komórką z pominięciem wiersza poniżej */
+		for(i=c-1 ; i < c+2 ; i++) { /* przeszukanie wiersza pod komórką z zawinieciem przy ostatnim wierszu */
 			if(i >= 0 && i < columns){
 				if(tablica[r+1][i] == 1){
 					ile_zywych++;
 				}
 			}
 		}
+	} else {
+		for(i=c-1 ; i < c+2 ; i++) { 
+                        if(i >= 0 && i < columns){
+                                if(tablica[0][i] == 1){
+                                        ile_zywych++;
+                                }
+                        }
+                }
 	}
 	
 	if((c-1) >= 0){
-		if(tablica[r][c-1] == 1){	     /* przeszukanie z lewej strony komórki z pominięciem wierszy -1 */	 
+		if(tablica[r][c-1] == 1){	     /* przeszukanie z lewej strony komórki z zawinieciem przy skrajnych lewych wierszach */	 
+			ile_zywych++;
+		}
+	} else {
+		if(tablica[r][columns-1] == 1){
 			ile_zywych++;
 		}
 	}
 
 	if((c+1) < columns ){
-		if(tablica[r][c+1] == 1){	     /* przeszukanie z prawej strony komórki z pominięciem komórki +1*/
+		if(tablica[r][c+1] == 1){	     /* przeszukanie z prawej strony komórki z zawinieciem przy skrajnych prawych wierszach */
 			ile_zywych++;
 		}
 
+	} else {
+		if(tablica[r][0] == 1){
+			ile_zywych++;
+		}
 	}
 
 	if(ile_zywych == 3){
